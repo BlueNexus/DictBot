6e71b2ee12780426deaf468e6936a2750c168404
BlueNexus <benreeve99@yahoo.co.uk>
2016-02-23 14:40:57 +0000

    Create config.py

---
+++ DictBot/config.py
@@ -0,0 +1,20 @@
+# # # # # # # # # # # # # # # # # # #
+# FILENAME = the path of the output file ("output.txt")
+#
+# AUTOIMPORT = whether or not the bot will automatically
+# import its list of known definitions on startup (True/False)
+# # # # # # # # # # # # # # # # # # #
+
+FILENAME = "output.txt"
+AUTOIMPORT = True
+
+# # # # # # # # # # # # # # # # # # #
+# DO NOT EDIT BELOW THIS LINE
+# -----------------------------------
+# # # # # # # # # # # # # # # # # # #
+
+def FILENAME():
+    return FILENAME
+
+def AUTOIMPORT():
+    return AUTOIMPORT
